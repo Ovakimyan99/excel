0e97125a64cfe1ec88c59358f914b2587b9192d5
tigran <tigr.kman@gmail.com>
2022-03-23 00:41:44 +0300

    вывод шаблонов компонентов

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { Excel } from '@/componetns/excel/Excel'
+import { Header } from '@/componetns/header/Header'
+import { Toolbar } from '@/componetns/toolbar/Toolbar'
+import { Formula } from '@/componetns/formula/Formula'
+import { Table } from '@/componetns/table/Table'
 import './scss/style.scss'
 
 const excel = new Excel('#app', {
-  components: []
+  components: [Header, Toolbar, Formula, Table]
 })
 
-console.log(excel)
+excel.render()
